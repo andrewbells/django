d96e075f0c166e334a369dd871be3b10bf304e2e
Andrey K <andkolok@gmail.com>
2013-10-08 22:25:17 +0400

    experiments w virtualenv

---
+++ projects/project_one/Scripts/easy_install-script.py
@@ -0,0 +1,10 @@
+#!"C:\Users\andko_000\Мои документы\github\django\projects\project_one\Scripts\python.exe" -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.9.8','console_scripts','easy_install'
+__requires__ = 'setuptools==0.9.8'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==0.9.8', 'console_scripts', 'easy_install')()
+    )
